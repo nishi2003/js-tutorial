a1525842f97a42b812867988b8c2b0bc7ffc0270
nishi2003 <nishinp2003@gmail.com>
2024-03-20 16:51:43 +0530

    optimise commit

--- star patterns/diamond-pattern.js
+++ star patterns/diamond-pattern.js
@@ -1,8 +1,7 @@
-
 let n = 5;
 for (let i = 0; i <= n; i++) {
     let str = " ";
-    for (let j = n; j >= i; j--) {
+    for (let j = i; j <=n; j++) {
         str += " ";
     }
     for (let k = 0; k<2*i-1; k++) {
@@ -23,4 +22,32 @@ for (let i = n-1; i>=1; i--) {
 }
 
 
+//optimise code
+
+// let n = 5;
+// for (let i = 0; i <= n; i++) {
+//     let str = " ";
+//     str = spacing(str,i,n);
+//     str = ptr(str,i);
+//     console.log(str);
+// }
+// for (let i = n-1; i>=1; i--) {
+//     let str = " ";
+//     str = spacing(str,i,n)
+//     str = ptr(str,i);
+//     console.log(str);
+// }
+// function spacing(str,i,n){
+//     for (let j = i; j <=n; j++) {
+//         str += " ";
+//     }
+//     return str;
+// }
+
+// function ptr(str,i){
+//     for (let k = 1; k <= 2*i-1; k++) {
+//         str += `${i-1}`;
+//     }
+//     return str;
+// }
 
